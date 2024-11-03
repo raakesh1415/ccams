0a636639d90b120c4ea2bbd32badccdf84d11ef0
Wong Ding Jiu <wongjiu@graduate.utm.my>
2024-11-03 16:58:12 +0800

    2

--- ccams/resources/views/registration/index.blade.php
+++ ccams/resources/views/registration/index.blade.php
@@ -7,20 +7,25 @@
         </div>
     </x-slot>
 
-    <div class="row">
-        @foreach ($categories as $category)
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    @if ($category->image_path)
-                        <img src="{{ asset($category->image_path) }}" class="card-img-top" alt="{{ $category->name }}">
-                    @endif
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $category->name }}</h5>
-                        <p class="card-text">{{ $category->description }}</p>
-                        <a href="{{ route('clubs.category', $category) }}" class="btn btn-primary">See More</a>
+    <div class="container">
+        <h1 class="mb-4">Club Type</h1>
+
+        <div class="row">
+            @foreach ($categories as $category)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        @if ($category->image_path)
+                            <img src="{{ asset($category->image_path) }}" class="card-img-top"
+                                alt="{{ $category->name }}">
+                        @endif
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $category->name }}</h5>
+                            <p class="card-text">{{ $category->description }}</p>
+                            <a href="{{ route('clubs.category', $category) }}" class="btn btn-primary">View Clubs</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 </x-layout>
