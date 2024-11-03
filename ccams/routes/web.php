f7c84b290b3e951fa24f4443d76b01857766f95a
Wong Ding Jiu <wongjiu@graduate.utm.my>
2024-11-03 22:27:55 +0800

    Update web.php

--- ccams/routes/web.php
+++ ccams/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('assessment.index');
 });
 
 Route::get('/assessment', function () {
