b8e4a521945b522a91f2c87a6e107f5734a9c923
Wong Ding Jiu <wongjiu@graduate.utm.my>
2024-11-06 22:06:31 +0800

    Update 2024_11_06_045440_create_registration_table.php

--- ccams/database/migrations/2024_11_06_045440_create_registration_table.php
+++ ccams/database/migrations/2024_11_06_045440_create_registration_table.php
@@ -3,17 +3,26 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-// php artisan migrate after this
 
 //Check
-return new class extends Migration
+class CreateRegistrationTable extends Migration
 {
 
-    public function up(): void
+    public function up()
     {
         Schema::create('registration', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('user_id');
+            $table->string('club_id');
+            $table->string('club_type');
+
+            //Foreign key
+            //OnDelete('cascade') ensures if a user is deleted, all associated registration will deleted
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('club_id')->references('id')->on('clubs')->onDelete('cascade');
+
+            //Ensures student can register one club for each club type
+            $table->unique(['user_id', 'club_type']);
         });
     }
 };
