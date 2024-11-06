21535e5ba2e3c8fe21d0d62d8685bb7b8b502f7b
Wong Ding Jiu <wongjiu@graduate.utm.my>
2024-11-06 22:33:01 +0800

    1

--- ccams/database/migrations/2024_11_06_045440_create_registrations_table.php
+++ ccams/database/migrations/2024_11_06_045440_create_registrations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-//Check
-class CreateRegistrationTable extends Migration
+class CreateRegistrationsTable extends Migration
 {
 
     public function up()
