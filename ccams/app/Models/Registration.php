11a6cb46557f41593f79ec2d45a921eef8545f7d
Wong Ding Jiu <wongjiu@graduate.utm.my>
2024-11-07 16:11:14 +0800

    Update Registration.php

--- ccams/app/Models/Registration.php
+++ ccams/app/Models/Registration.php
@@ -8,6 +8,8 @@ class Registration extends Model
 {
     //Fillable field
     //Can assign multiple attributes to a model instance at once
+
+    protected $table = 'registrations';
     protected $fillable = [
         'user_id',
         'club_id',
