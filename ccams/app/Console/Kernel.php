ade9380726e8ef5533c2368bca44e6b185ac5264
Wong Ding Jiu <wongjiu@graduate.utm.my>
2024-12-17 20:24:01 +0800

    Update Kernel.php

--- ccams/app/Console/Kernel.php
+++ ccams/app/Console/Kernel.php
@@ -1,4 +1,16 @@
-protected function schedule(Schedule $schedule)
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
 {
-    $schedule->command('clubs:auto-assign')->daily(); // Runs daily
+    protected function schedule(Schedule $schedule)
+    {
+        // Schedule the auto-assign clubs command to run daily
+        $schedule->command('clubs:auto-assign')->daily(); // Runs every day at midnight
+    
+    }
 }
