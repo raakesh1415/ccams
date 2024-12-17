d730b2bb5eaff1c1bdef09ee963638104aa94d39
Erfan Syabil <muhderfan2610@gmail.com>
2024-12-18 01:33:22 +0800

    Add Bootstrap v4 to layout.blade.php

--- ccams/resources/views/components/layout.blade.php
+++ ccams/resources/views/components/layout.blade.php
@@ -11,8 +11,8 @@
     <title>CCAMS</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css"
-        rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
 </head>
