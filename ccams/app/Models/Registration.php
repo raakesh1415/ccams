4df56ffa71a6244e60202b970fd8fb3002318b54
Raakesh <raakesh1415@gmail.com>
2024-12-21 15:25:39 +0800

    update assessment

--- ccams/app/Models/Registration.php
+++ ccams/app/Models/Registration.php
@@ -28,7 +28,7 @@ class Registration extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id', 'user_id');
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     public function assessments()
